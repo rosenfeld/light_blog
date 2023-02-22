63c9393797c591a3a1b49f7a35934d633cdfd052
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2023-02-22 14:51:12 -0300

    Move ContentProcessor to the LightBlog module

--- lib/light_blog/content_processor.rb
+++ lib/light_blog/content_processor.rb
@@ -2,24 +2,26 @@
 
 require "erubi"
 
-# Process ERB articles when config allows it, provides static_path helper.
-# This is useful for setting some image paths and other static assets
-class ContentProcessor
-  def initialize(config, content)
-    @config = config
-    @content = content
-  end
+module LightBlog
+  # Process ERB articles when config allows it, provides static_path helper.
+  # This is useful for setting some image paths and other static assets
+  class ContentProcessor
+    def initialize(config, content)
+      @config = config
+      @content = content
+    end
 
-  def render
-    eval Erubi::Engine.new(@content).src
-  rescue StandardError => e
-    "<p>Invalid article ERB source: #{e.message}.</p><div><pre>#{e.backtrace.join("\n")}</pre></div>"
-  end
+    def render
+      eval Erubi::Engine.new(@content).src
+    rescue StandardError => e
+      "<p>Invalid article ERB source: #{e.message}.</p><div><pre>#{e.backtrace.join("\n")}</pre></div>"
+    end
 
-  # helpers
+    # helpers
 
-  def static_path(path)
-    @static_path ||= [@config.base_mount_path, @config.articles_static_mount_path].join
-    [@static_path, path].join("/")
+    def static_path(path)
+      @static_path ||= [@config.base_mount_path, @config.articles_static_mount_path].join
+      [@static_path, path].join("/")
+    end
   end
 end
